3496262534699de7d307f55deef436894f4bc492
jeol11 <jeol12369@gmail.com>
2023-08-28 03:06:15 +0100

    nw

--- store/store.js
+++ store/store.js
@@ -1,7 +1,7 @@
 'use client'
 import {configureStore } from '@reduxjs/toolkit'
 import wishlistReducer from './wishlistSlice'
-
+  
 export const store = configureStore({
   reducer:{
     wishlist:wishlistReducer,
